b848d1fed024abd92b468abf2018badee3240b96
Franklin Davis <fdapjkdevops@gmail.com>
2023-07-29 15:45:43 -0400

    Files added for a simple VM.

---
+++ 0100-singlevm/terraform/libvirt.tf
@@ -0,0 +1,45 @@
+# 2. ----> We fetch the smallest ubuntu image from the cloud image repo
+resource "libvirt_volume" "ubuntu-jammy-qcow2" {
+  name   = "ubuntujammy.qcow2"
+  pool   = "default" ## ---> This should be same as your disk pool name
+ #source = "https://cloud-images.ubuntu.com/jammy/20230720/jammy-server-cloudimg-amd64.img"
+  source = "file:///var/lib/libvirt/images/ubuntu-2204base.qcow2"
+  format = "qcow2"
+}
+
+# Use CloudInit to add our ssh-key to the instance
+resource "libvirt_cloudinit_disk" "commoninit" {
+ name           = "commoninit0100-vm.iso"
+ pool           = "default" #CHANGEME
+ user_data      = data.template_file.user_data.rendered
+}
+
+data "template_file" "user_data" {
+ template = file("${path.module}/cloud_init0100vm.cfg")
+}
+
+# 5. -----> Create the compute vm
+resource "libvirt_domain" "ubuntu0100vm" {
+  name   = "ubuntu0100vm"
+  memory = "4096"
+  vcpu   = 2
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id  
+  network_interface {
+     network_name = "default" ## ---> This should be the same as your network name 
+  }
+
+  console { # ----> define a console for the domain.
+     type        = "pty"
+     target_port = "0"
+     target_type = "serial" 
+  }
+  disk {   
+     volume_id = "${libvirt_volume.ubuntu-jammy-qcow2.id}"
+  } # ----> map/attach the disk 
+  graphics { ## ---> graphics settings
+     type        = "spice"
+     listen_type = "address"
+     autoport    = "true"
+  }
+}
