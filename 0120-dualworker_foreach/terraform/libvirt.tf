3474ab1d663824693f591388bc4ea2ef7572916a
Franklin Davis <fdapjkdevops@gmail.com>
2023-07-31 18:44:26 -0400

    Added cloudInit so that root is allowed and has password.

--- 0120-dualworker_foreach/terraform/libvirt.tf
+++ 0120-dualworker_foreach/terraform/libvirt.tf
@@ -6,6 +6,13 @@ resource "libvirt_volume" "jammy_image" {
   format = "qcow2"
 }
 
+# Use CloudInit to add our ssh-key to the instance
+resource "libvirt_cloudinit_disk" "commoninit" {
+ name           = "commoninitworker.iso"
+ pool           = "default" #CHANGEME
+ user_data      = data.template_file.user_data.rendered
+}
+
 data "template_file" "user_data" {
   template = file("${path.module}/cloud_init.cfg")
 }
@@ -13,19 +20,17 @@ data "template_file" "user_data" {
 resource "libvirt_volume" "worker" {
    for_each = var.worker_names
    name = "worker_${each.value}.qcow2"
-#   name = worker_${each.value}.qcow2
    base_volume_id = libvirt_volume.jammy_image.id
 }
 
 # 5. -----> Create the compute vm
 resource "libvirt_domain" "ubi_0120vm" {
-#  name   = "ubuntu0100vm"
   for_each = var.worker_names
   name = each.value
   memory = var.worker_mem
   vcpu   = var.worker_vcpu
 
-#  cloudinit = libvirt_cloudinit_disk.commoninit.id  
+  cloudinit = libvirt_cloudinit_disk.commoninit.id  
   network_interface {
      network_name = "default" ## ---> This should be the same as your network name 
   }
@@ -35,17 +40,8 @@ resource "libvirt_domain" "ubi_0120vm" {
      target_port = "0"
      target_type = "serial" 
   }
-  #disk {   
-  #   volume_id = "${libvirt_volume.ubuntu-jammy-qcow2.id}"
-  #} # ----> map/attach the disk
 
-  #disk {
-  #  volume_id = libvirt_volume.volume.id
-  # } 
   disk  {
-     #volume_id = "libvirt_volume.worker_[each.value].id"
-     #volume_id = libvirt_volume.worker_[each.value].id
-     #volume_id = libvirt_volume.worker.id   # got the one with[each.key]
      volume_id = libvirt_volume.worker[each.value].id
   }
 
