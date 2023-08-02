5282a338adc72d7a5de5230b0bccff31b3f004ae
Franklin Davis <fdapjkdevops@gmail.com>
2023-08-02 16:59:59 -0400

    Creating master and 2 workers with terraform. Will be provisioned with ansible.

--- 0120-dualworker_foreach/terraform/libvirt.tf
+++ 0120-dualworker_foreach/terraform/libvirt.tf
@@ -1,29 +1,55 @@
-# 2. ----> We fetch the smallest ubuntu image from the cloud image repo
-resource "libvirt_volume" "jammy_image" {
+# Image for workers.   These are from the raw ubuntu, unexpanded
+# 
+resource "libvirt_volume" "jammy_worker_image" {
   name   = "ubuntujammy.qcow2"
   pool   = "default" ## ---> This should be same as your disk pool name
   source = "file:///var/lib/libvirt/images/ubuntu-2204base.qcow2"
   format = "qcow2"
 }
 
-# Use CloudInit to add our ssh-key to the instance
-resource "libvirt_cloudinit_disk" "commoninit" {
- name           = "commoninitworker.iso"
- pool           = "default" #CHANGEME
- user_data      = data.template_file.user_data.rendered
+# Image for masters.   These are from an expanded raw image (ubuntu-master.qcow2)
+resource "libvirt_volume" "jammy_master_image" {
+  name   = "ubuntui_master_image.qcow2"
+  pool   = "default" ## ---> This should be same as your disk pool name
+  source = "file:///var/lib/libvirt/images/ubuntu-master.qcow2"
+  format = "qcow2"
 }
 
-data "template_file" "user_data" {
-  template = file("${path.module}/cloud_init.cfg")
+# iso for worker user_data
+resource "libvirt_cloudinit_disk" "commoninitworker" {
+ name           = "commoninitworker.iso"
+ pool           = "default" #CHANGEME
+ user_data      = data.template_file.user_worker_data.rendered
+}
+
+# iso for master user_data
+resource "libvirt_cloudinit_disk" "commoninitmaster" {
+ name           = "commoninitmaster.iso"
+ pool           = "default" #CHANGEME
+ user_data      = data.template_file.user_master_data.rendered
+}
+
+data "template_file" "user_worker_data" {
+  template = file("${path.module}/cloud_worker_init.cfg")
+}
+
+data "template_file" "user_master_data" {
+  template = file("${path.module}/cloud_master_init.cfg")
 }
 
 resource "libvirt_volume" "worker" {
    for_each = var.worker_names
    name = "worker_${each.value}.qcow2"
-   base_volume_id = libvirt_volume.jammy_image.id
+   base_volume_id = libvirt_volume.jammy_worker_image.id
 }
 
-resource "libvirt_domain" "ubi_0120vm" {
+resource "libvirt_volume" "master" {
+   for_each = var.master_names
+   name = "master_${each.value}.qcow2"
+   base_volume_id = libvirt_volume.jammy_master_image.id
+}
+
+resource "libvirt_domain" "ubi_0120vmworker" {
   for_each = var.worker_names
   name = each.value
   memory = var.worker_mem
@@ -34,7 +60,7 @@ resource "libvirt_domain" "ubi_0120vm" {
   # CLOUDINIT SECTION
   #
   #--------------------------------------------------------------
-  cloudinit = libvirt_cloudinit_disk.commoninit.id  
+  cloudinit = libvirt_cloudinit_disk.commoninitworker.id  
   network_interface {
      network_name = "default" ## ---> This should be the same as your network name 
   }
@@ -47,6 +73,41 @@ resource "libvirt_domain" "ubi_0120vm" {
 
   disk  {
      volume_id = libvirt_volume.worker[each.value].id
+  }
+
+
+  graphics { ## ---> graphics settings
+     type        = "spice"
+     listen_type = "address"
+     autoport    = "true"
+  }
+}
+
+# Masters
+resource "libvirt_domain" "ubi_0120vmmaster" {
+  for_each = var.master_names
+  name = each.value
+  memory = var.master_mem
+  vcpu   = var.master_vcpu
+
+  #--------------------------------------------------------------
+  #
+  # CLOUDINIT SECTION
+  #
+  #--------------------------------------------------------------
+  cloudinit = libvirt_cloudinit_disk.commoninitmaster.id  
+  network_interface {
+     network_name = "default" ## ---> This should be the same as your network name 
+  }
+
+  console { # ----> define a console for the domain.
+     type        = "pty"
+     target_port = "0"
+     target_type = "serial" 
+  }
+
+  disk  {
+     volume_id = libvirt_volume.master[each.value].id
   }
 
 
--- 0120-dualworker_foreach/terraform/variables.tf
+++ 0120-dualworker_foreach/terraform/variables.tf
@@ -2,23 +2,38 @@
 # variables.tf:
 #    This is the declaration of the variables in scope.
 
+
 variable "VMWorkers" {
-    default = "1"
+    default = "2"
 }
 
 variable "worker_names" {
 #   type = list(string)
    type = set(string)
-   default = []
+   
+   default = ["worker1","worker2"]
    description="Worker VMs to create"
 }
 
 variable "worker_vcpu" {
-   default = "1"
+   default = "2"
 }
 
 variable "worker_mem" {
    default = "512"
 }
 
+variable "master_names" {
+   type = set(string)
+   default = ["master1","master2"]
+   description="Master VMs to create"
+}
+
+variable "master_vcpu" {
+   default = "1"
+}
+
+variable "master_mem" {
+   default = "1024"
+}
 
