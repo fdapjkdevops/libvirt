fb0f254d7fb0136ac7df9d93d3a952adef6ca723
Franklin Davis <fdapjkdevops@gmail.com>
2023-08-17 21:46:02 -0400

    Using for_each for master and worker.

---
+++ 0120-dualworker_foreach/terraform/getimage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+#
+#https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+
+#
+# Actually downloaded with:
+if [ -e "jammy-server-cloudimg-amd64.img" ]; then
+   echo Already there
+else
+#    curl https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64-disk-kvm.img \
+#        --output 01-jammy-server-cloudimg-amd64-disk-kvm.img
+#    curl https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img \
+#        --output 02-jammy-server-cloudimg-amd64.img
+    curl https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img \
+        --output jammy-server-cloudimg-amd64.img
+fi
+#
+# Converted the raw image into a qcow
+# This will ID the type, need to script this ; qemu-img info jammy-server-cloudimg-amd64.img 
+#qemu-img convert -f raw -O qcow2 jammy-server-cloudimg-amd64.img jammy-server-cloudimg-amd64.qcow2
+#
+
+# delete the qcow2 if present
+if [ -e jammy-server-cloudimg-amd64.qcow2 ]; then
+   rm jammy-server-cloudimg-amd64.qcow2
+fi
+cp jammy-server-cloudimg-amd64.img   jammy-server-cloudimg-amd64.qcow2
+cp jammy-server-cloudimg-amd64.qcow2 ubuntu-2204base.qcow2
+cp jammy-server-cloudimg-amd64.qcow2 ubuntu-master.qcow2
+#
+# Increase the master size by 5GB
+qemu-img resize ubuntu-master.qcow2 +5G
--- 0120-dualworker_foreach/terraform/libvirt.tf
+++ 0120-dualworker_foreach/terraform/libvirt.tf
@@ -1,90 +1,38 @@
-# Image for workers.   These are from the raw ubuntu, unexpanded
-# 
-resource "libvirt_volume" "jammy_worker_image" {
-  name   = "ubuntujammy.qcow2"
+# ISO for the master instances
+resource "libvirt_volume" "ubuntu-master-image" {
+  name   = "ubuntujammymaster.qcow2"
+  pool   = "default" ## ---> This should be same as your disk pool name
+  source = "file:///var/lib/libvirt/images/ubuntu-master.qcow2"
+#  source = "file:///var/lib/libvirt/images/ubuntu-2204base.qcow2"
+  format = "qcow2"
+}
+
+resource "libvirt_volume" "ubuntu-worker-image" {
+  name   = "ubuntujammyworker.qcow2"
   pool   = "default" ## ---> This should be same as your disk pool name
   source = "file:///var/lib/libvirt/images/ubuntu-2204base.qcow2"
   format = "qcow2"
 }
 
-# Image for masters.   These are from an expanded raw image (ubuntu-master.qcow2)
-resource "libvirt_volume" "jammy_master_image" {
-  name   = "ubuntui_master_image.qcow2"
-  pool   = "default" ## ---> This should be same as your disk pool name
-  source = "file:///var/lib/libvirt/images/ubuntu-master.qcow2"
-  format = "qcow2"
-}
-
-# iso for worker user_data
-resource "libvirt_cloudinit_disk" "commoninitworker" {
- name           = "commoninitworker.iso"
- pool           = "default" #CHANGEME
- user_data      = data.template_file.user_worker_data.rendered
-}
-
-# iso for master user_data
+# User_DATA ISO for the master numbered
 resource "libvirt_cloudinit_disk" "commoninitmaster" {
- name           = "commoninitmaster.iso"
+ name           = "commoninitmast-vmN.iso"
  pool           = "default" #CHANGEME
- user_data      = data.template_file.user_master_data.rendered
+ user_data      = data.template_file.user_data_master.rendered
 }
 
-data "template_file" "user_worker_data" {
-  template = file("${path.module}/cloud_worker_init.cfg")
-}
-
-data "template_file" "user_master_data" {
-  template = file("${path.module}/cloud_master_init.cfg")
-}
-
-resource "libvirt_volume" "worker" {
-   for_each = var.worker_names
-   name = "worker_${each.value}.qcow2"
-   base_volume_id = libvirt_volume.jammy_worker_image.id
+data "template_file" "user_data_master" {
+ template = file("${path.module}/cloud_initmast.cfg")
 }
 
 resource "libvirt_volume" "master" {
    for_each = var.master_names
    name = "master_${each.value}.qcow2"
-   base_volume_id = libvirt_volume.jammy_master_image.id
-}
-
-resource "libvirt_domain" "ubi_0120vmworker" {
-  for_each = var.worker_names
-  name = each.value
-  memory = var.worker_mem
-  vcpu   = var.worker_vcpu
-
-  #--------------------------------------------------------------
-  #
-  # CLOUDINIT SECTION
-  #
-  #--------------------------------------------------------------
-  cloudinit = libvirt_cloudinit_disk.commoninitworker.id  
-  network_interface {
-     network_name = "default" ## ---> This should be the same as your network name 
-  }
-
-  console { # ----> define a console for the domain.
-     type        = "pty"
-     target_port = "0"
-     target_type = "serial" 
-  }
-
-  disk  {
-     volume_id = libvirt_volume.worker[each.value].id
-  }
-
-
-  graphics { ## ---> graphics settings
-     type        = "spice"
-     listen_type = "address"
-     autoport    = "true"
-  }
+   base_volume_id = libvirt_volume.ubuntu-master-image.id
 }
 
 # Masters
-resource "libvirt_domain" "ubi_0120vmmaster" {
+resource "libvirt_domain" "ubi0120vmmaster" {
   for_each = var.master_names
   name = each.value
   memory = var.master_mem
@@ -116,5 +64,74 @@ resource "libvirt_domain" "ubi_0120vmmaster" {
      listen_type = "address"
      autoport    = "true"
   }
+
+  connection {
+     type     = "ssh"
+     user     = "root"
+     password = "unix1234"
+     # host     = "${var.host}"
+     host = each.value
+  }
+
 }
 
+# User_DATA ISO for the worker numbered
+resource "libvirt_cloudinit_disk" "commoninitworker" {
+ name           = "commoninitwork-vmN.iso"
+ pool           = "default" #CHANGEME
+ user_data      = data.template_file.user_data_worker.rendered
+}
+
+data "template_file" "user_data_worker" {
+ template = file("${path.module}/cloud_initwork.cfg")
+}
+
+resource "libvirt_volume" "worker" {
+   for_each = var.worker_names
+   name = "worker_${each.value}.qcow2"
+   base_volume_id = libvirt_volume.ubuntu-worker-image.id
+}
+
+# Workers
+resource "libvirt_domain" "ubi0120vmworker" {
+  for_each = var.worker_names
+  name = each.value
+  memory = var.worker_mem
+  vcpu   = var.worker_vcpu
+
+  #--------------------------------------------------------------
+  #
+  # CLOUDINIT SECTION
+  #
+  #--------------------------------------------------------------
+  cloudinit = libvirt_cloudinit_disk.commoninitworker.id  
+  network_interface {
+     network_name = "default" ## ---> This should be the same as your network name 
+  }
+
+  console { # ----> define a console for the domain.
+     type        = "pty"
+     target_port = "0"
+     target_type = "serial" 
+  }
+
+  disk  {
+     volume_id = libvirt_volume.worker[each.value].id
+  }
+
+
+  graphics { ## ---> graphics settings
+     type        = "spice"
+     listen_type = "address"
+     autoport    = "true"
+  }
+
+  connection {
+     type     = "ssh"
+     user     = "root"
+     password = "unix1234"
+     # host     = "${var.host}"
+     host = each.value
+  }
+
+}
