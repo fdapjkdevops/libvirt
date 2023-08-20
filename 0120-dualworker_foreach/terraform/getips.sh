ef84523a64191c7d0730b14236d3c3f5f5008395
Franklin Davis <fdapjkdevops@gmail.com>
2023-08-20 13:50:51 -0400

    Modified getips.sh so the IP for each VM will be substituted in the ansible inventory file.

--- 0120-dualworker_foreach/terraform/getips.sh
+++ 0120-dualworker_foreach/terraform/getips.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
 # Below will get the mac address of master and place into $themac
 #themac=$(virsh dumpxml master | grep "mac address" | awk -F\' ' { print $2}')
 namelist="master worker1 worker2"
 rm -f macaddresses
 rm -f ipvalues.txt
+rm -f ../ansible/hosts
+cp ../ansible/hosts.template ../ansible/hosts > null
 touch macaddresses
 for name in $namelist; do
    mac=$(virsh dumpxml $name | grep "mac address" | awk -F\' ' { print $2}') 
@@ -13,6 +15,12 @@ for name in $namelist; do
    ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$ipline")"
    echo IP Address for $name is: $ip
    echo $ip >>ipvalues.txt
+   uname=${name^^}
+   uname={{${name^^}IP}}
+   substr=s/$uname/$ip/g
+   #echo uname: $uname -\> $ip
+   #echo substr: $substr
+   sed -i $substr ../ansible/hosts
 done
 
 
